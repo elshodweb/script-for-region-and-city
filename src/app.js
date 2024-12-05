ab4127a55a30daed030b5b80ca5e3016e188d8de
elshod tukhtamurodov <elshodweb@gmail.com>
2024-12-05 14:45:41 +0500

    remove comments

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
-const API_URL = 'http://localhost:3000/v1'; // Replace with your API URL
-const TOKEN = ''; // Replace with your token
+const API_URL = 'http://localhost:3000/v1'; 
+const TOKEN = ''; 
 
-// Define the structure for City and Region
+
 const regions = JSON.parse(fs.readFileSync(path.join('src', 'data', 'index.json'), 'utf-8'));
 
 const headers = {
@@ -14,20 +14,20 @@ const headers = {
     'Content-Type': 'application/json',
 };
 
-// Function to create a region
+
 async function createRegion(region) {
     try {
         const response = await axios.post(`${API_URL}/region`, region, { headers });
         console.log(`Region created: ${region.name.ru}`);
 
-        return response.data.result.id; // Assuming the API returns the region id
+        return response.data.result.id; 
     } catch (error) {
         console.error(`Error creating region ${region.name.ru}:`, error);
         return null;
     }
 }
 
-// Function to create a city in the specified region
+
 async function createCity(regionId, city) {
     try {
         const response = await axios.post(`${API_URL}/city`, {
@@ -40,7 +40,7 @@ async function createCity(regionId, city) {
     }
 }
 
-// Function to seed data (create regions and cities)
+
 async function seedData() {
     for (const region of regions) {
         const regionId = await createRegion(region);
@@ -53,5 +53,5 @@ async function seedData() {
     console.log('successfully finished');
 }
 
-// Run the seed data process
+
 seedData();
